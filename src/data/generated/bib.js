1262475348630e2c25f9f97744a0af3f513f2227
psxia11 <psxia11@nottingham.ac.uk>
2026-05-11 04:47:26 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -1,17 +1,100 @@
-﻿const generatedBibEntries = {
-    "Beck2016Visual": {
-        "abstract": "Bibiographic data such as collections of scientific articles and citation networks have been studied extensively in information visualization and visual analytics research. Powerful systems have been built to support various types of bibliographic analysis, but they require some training and cannot be used to disseminate the insights gained. In contrast, we focused on developing a more accessible visual analytics system, called SurVis, that is ready to disseminate a carefully surveyed literature collection. The authors of a survey may use our Web-based system to structure and analyze their literature database. Later, readers of the survey can obtain an overview, quickly retrieve specific publications, and reproduce or extend the original bibliographic analysis. Our system employs a set of selectors that enable users to filter and browse the literature collection as well as to control interactive visualizations. The versatile selector concept includes selectors for textual search, filtering by keywords and meta-information, selection and clustering of similar publications, and following citation links. Agreement to the selector is represented by word-sized sparkline visualizations seamlessly integrated into the user interface. Based on an analysis of the analytical reasoning process, we derived requirements for the system. We developed the system in a formative way involving other researchers writing literature surveys. A questionnaire study with 14 visual analytics experts confirms that SurVis meets the initially formulated requirements.",
-        "author": "Beck, Fabian and Koch, Sebastian and Weiskopf, Daniel",
-        "doi": "10.1109/TVCG.2015.2467757",
-        "journal": "IEEE Transactions on Visualization and Computer Graphics",
-        "keywords": "type:system, visual_analytics, sparklines, information_retrieval, clustering, literature_browser",
-        "number": "01",
-        "publisher": "IEEE",
-        "series": "TVCG",
-        "title": "Visual Analysis and Dissemination of Scientific Literature Collections with {SurVis}",
-        "type": "article",
-        "url": "http://www.visus.uni-stuttgart.de/uploads/tx_vispublications/vast15-survis.pdf",
-        "volume": "22",
-        "year": "2016"
-    }
+var generatedBibEntries = {
+	"Lewis2020Retrieval": {
+		"author": "Lewis, Patrick and Perez, Ethan and Piktus, Aleksandra and Petroni, Fabio and Karpukhin, Vladimir and Goyal, Naman and K{\\\"u}ttler, Heinrich and Lewis, Mike and Yih, Wen-tau and Rockt{\\\"a}schel, Tim and Riedel, Sebastian and Kiela, Douwe",
+		"booktitle": "Advances in Neural Information Processing Systems",
+		"doi": "10.5555/3495724.3496517",
+		"keywords": "type:retrieval-based-memory, external_memory",
+		"pages": "9459--9474",
+		"title": "Retrieval-Augmented Generation for Knowledge-Intensive {NLP} Tasks",
+		"type": "inproceedings",
+		"volume": "33",
+		"year": "2020"
+	},
+	"Borgeaud2022Improving": {
+		"author": "Borgeaud, Sebastian and Mensch, Arthur and Hoffmann, Jordan and Cai, Trevor and Rutherford, Eliza and Millican, Katie and van den Driessche, George and Paganini, Michela and Irving, Geoffrey and Vinyals, Oriol and Simonyan, Karen and Rae, Jack and Elsen, Erich and Sifre, Laurent",
+		"booktitle": "Proceedings of the 39th International Conference on Machine Learning",
+		"doi": "10.48550/arXiv.2112.04426",
+		"keywords": "type:retrieval-based-memory, large-scale_retrieval",
+		"pages": "2206--2240",
+		"title": "Improving Language Models by Retrieving from Trillions of Tokens",
+		"type": "inproceedings",
+		"year": "2022"
+	},
+	"Wu2022Memorizing": {
+		"author": "Wu, Yuhuai and Rabe, Markus N. and Hutchins, DeLesley and Szegedy, Christian",
+		"booktitle": "International Conference on Learning Representations",
+		"doi": "10.48550/arXiv.2203.08913",
+		"keywords": "type:retrieval-based-memory, kNN_memory",
+		"title": "Memorizing Transformers",
+		"type": "inproceedings",
+		"year": "2022"
+	},
+	"Yao2023ReAct": {
+		"author": "Yao, Shunyu and Zhao, Jeffrey and Yu, Dian and Du, Nan and Shafran, Izhak and Narasimhan, Karthik and Cao, Yuan",
+		"booktitle": "International Conference on Learning Representations",
+		"doi": "10.48550/arXiv.2210.03629",
+		"keywords": "type:episodic-memory, agent_reasoning",
+		"title": "{ReAct}: Synergizing Reasoning and Acting in Language Models",
+		"type": "inproceedings",
+		"year": "2023"
+	},
+	"Park2023Generative": {
+		"author": "Park, Joon Sung and O'Brien, Joseph C. and Cai, Carrie Jun and Morris, Meredith Ringel and Liang, Percy and Bernstein, Michael S.",
+		"booktitle": "Proceedings of the 36th Annual ACM Symposium on User Interface Software and Technology",
+		"doi": "10.1145/3586183.3606763",
+		"keywords": "type:episodic-memory, memory_stream, multi-agent",
+		"pages": "1--22",
+		"title": "Generative Agents: Interactive Simulacra of Human Behavior",
+		"type": "inproceedings",
+		"year": "2023"
+	},
+	"Packer2023MemGPT": {
+		"author": "Packer, Charles and Fang, Vivian and Patil, Shishir G. and Lin, Kevin and Wooders, Sarah and Gonzalez, Joseph E.",
+		"doi": "10.48550/arXiv.2310.08560",
+		"journal": "arXiv preprint arXiv:2310.08560",
+		"keywords": "type:persistent-memory-management, hierarchical_memory, context_window",
+		"title": "{MemGPT}: Towards {LLMs} as Operating Systems",
+		"type": "article",
+		"year": "2023"
+	},
+	"Shinn2023Reflexion": {
+		"author": "Shinn, Noah and Cassano, Federico and Gopinath, Ashwin and Narasimhan, Karthik and Yao, Shunyu",
+		"booktitle": "Advances in Neural Information Processing Systems",
+		"doi": "10.5555/3666122.3666499",
+		"keywords": "type:episodic-memory, reflective_memory, reinforcement_learning",
+		"title": "Reflexion: Language Agents with Verbal Reinforcement Learning",
+		"type": "inproceedings",
+		"volume": "36",
+		"year": "2023"
+	},
+	"Sumers2024Cognitive": {
+		"author": "Sumers, Theodore R. and Yao, Shunyu and Narasimhan, Karthik and Griffiths, Thomas L.",
+		"doi": "10.48550/arXiv.2309.02427",
+		"journal": "Transactions on Machine Learning Research",
+		"keywords": "type:survey, cognitive_architecture, memory_taxonomy",
+		"title": "Cognitive Architectures for Language Agents",
+		"type": "article",
+		"year": "2024"
+	},
+	"Zhong2024MemoryBank": {
+		"author": "Zhong, Wanjun and Guo, Lianghong and Gao, Qiqi and Ye, He and Wang, Yanlin",
+		"booktitle": "Proceedings of the AAAI Conference on Artificial Intelligence",
+		"doi": "10.1609/aaai.v38i17.29946",
+		"keywords": "type:persistent-memory-management, long-term_memory, forgetting_curve",
+		"number": "17",
+		"pages": "19724--19731",
+		"title": "{MemoryBank}: Enhancing Large Language Models with Long-Term Memory",
+		"type": "inproceedings",
+		"volume": "38",
+		"year": "2024"
+	},
+	"Zhang2024Survey": {
+		"author": "Zhang, Zeyu and Bo, Xiaohe and Ma, Chen and Li, Rui and Chen, Xu and Dai, Quanyu and Zhu, Jieming and Dong, Zhenhua and Wen, Ji-Rong",
+		"doi": "10.1145/3748302",
+		"journal": "ACM Transactions on Information Systems",
+		"keywords": "type:survey, memory_mechanism, LLM_agents",
+		"title": "A Survey on the Memory Mechanism of Large Language Model based Agents",
+		"type": "article",
+		"year": "2024"
+	}
 };
